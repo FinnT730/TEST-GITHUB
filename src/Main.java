979d3c41ed4cbe04e01ed7ebcef13a8e7e545e6a
FinnT730 <finnt730@gmail.com>
2016-03-30 22:33:55 +0200

    Update the background, this is just an test!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
@@ -14,26 +17,29 @@ public class Main extends Application{
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		stage.show();		
-		stage.setTitle("Editor");
-		
-		Image image = new Image("test.png");
-		
-		GridPane gp = new GridPane();
-		
-		Group root = new Group();
-		
-		
-		
-		
-		Scene s = new Scene(root);
-		
-		
-		
-		
-		stage.setScene(s);
-		
-		// stage.setResizable(true);
+		stage.setTitle( "Canvas Example" );
+        
+	    Group root = new Group();
+	    Scene theScene = new Scene( root );
+	    
+	    
+	    Image img = new Image("https://pbs.twimg.com/tweet_video/Ce0lJYdXEAAYzct.mp4");
+	    
+	    GridPane pane = new GridPane();
+	    
+	    ProgressBar ps = new ProgressBar(100);
+	    
+	    if(ps.getProgress() != 1000) {
+	    	ps.setProgress(ps.getProgress() + 1);
+	    }
+	    
+	    pane.setBackground(new BackgroundImage(new BackgroundImage(img, 100, 100, 100, 100),0, 0, 100, 100));
+	    
+	    pane.getChildren().add(ps);
+	    
+	    stage.setScene(new Scene(pane));
+	         
+	    stage.show();
 	}
 
 
